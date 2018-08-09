e0ab510deb3a3e8c70d93968a60502a2ae10657d
zhenghq <zhenghq@nationalchip.com>
2018-08-09 21:58:40 +0800

    extra data

--- auth.py
+++ auth.py
@@ -4,6 +4,7 @@ from log import Logger
 
 test_room='test'
 runing_club='珞珈跑团'
+mark = '#run'
 
 logger = Logger(__name__).log()
 
@@ -15,22 +16,38 @@ def logout():
     itchat.logout()
 
 def reply(filter_fn,msg):
+    logger.debug(msg)
     result = filter_fn(msg)
     if not result:
-        logger.debug(msg)
         return None
 
     else:
         text = msg['Text']
         logger.debug('receive text: %s' % text)
 
-        if(text.startswith('#')):
+
+        if(text.startswith(mark)):
+            text = text.replace(mark,'').strip()
+            data = extra(text)
+            print(data)
             return '您输入了话题： %s' % text
 
 def extra(text):
-    reg = '(\d*.?\d+)/?'
-    keywords = re.findall(reg,text)
-    return keywords
+    reg = '[\d./]*'
+    records = re.findall(reg,text)
+    print(records)
+    for record in records:
+        if re.match('\d\.|/',record)!=None or record == '' or len(record) == 0:
+            continue
+        else:
+            name = text[0:text.index(record)].strip()
+            cols = record.split('/')
+            for col in cols:
+                if col=='':
+                    cols.remove(col)
+            print(name,cols)
+            return name,cols
+    return None
 
 
 
@@ -50,12 +67,20 @@ def text_reply(msg):
 
 
 if __name__=='__main__':
-    # itchat.auto_login()
-    # itchat.run(debug=True,blockThread=True)
+    itchat.auto_login()
+    itchat.run(debug=True,blockThread=True)
 
-    text='1.分结束啦20.3/49.3/150.3'
-    keys = extra(text)
-    print(keys)
+    # text='#run分结束啦20.3/49.3/150.3'
+    # keys = extra(text)
+
+
+    # text='#run得劲20.3/49.3/150.3'
+    # keys = extra(text)
+
+
+    # text='#的分结束啦20.3/ / /'
+    # keys = extra(text)
+
 
 
 
